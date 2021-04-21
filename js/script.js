1a5108bbe9617f4f41d3ec88312931318af9260b
James Vongampai <jaythomasv29@gmail.com>
2021-04-21 13:07:13 -0700

    add javascript logic to work with datastructures and functions to add random object to html document

--- js/script.js
+++ js/script.js
@@ -1,33 +1,84 @@
 /******************************************
-Treehouse FSJS Techdegree:
+Treehouse FSJS Techdegree: James Vongampai
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /***
+ * `quotes` array
+ ***/
+ const quotes = [
+  {
+    quote:
+      "All our dreams can come true, if we have the courage to pursue them.",
+    source: "Walt Disney",
+    citation: "Facebook",
+    year: "1991",
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    source: "Mark Twain",
+    citation: "Twitter",
+    year: "1984",
+  },
+  {
+    quote: "I wake up every morning and think to myself, ‘how far can I push this company in the next 24 hours.’",
+    source: "Leah Busque",
+    citation: "",
+    year: "1946",
+  },
+  {
+    quote: "Whatever you are, be a good one.",
+    source: "Abraham Lincoln",
+    citation: "",
+    year: "",
+  },
+  {
+    quote: "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.",
+    source: "Neil Gaiman",
+    citation: "",
+    year: '',
+  }
+];
+/***
  * `getRandomQuote` function
-***/
+ ***/
 
-
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)]; // returns an element between quotes[0-4]
+}
 
 /***
  * `printQuote` function
-***/
+ ***/
+function printQuote() {
+  const quoteBox = document.querySelector("#quote-box");
+  const randomQuote = getRandomQuote(); // stores random quote as variable
+  let newQuote = "";
 
+  newQuote += `<p class="quote">${randomQuote.quote}</p>`;
 
+  //Check for source(s)
+
+  newQuote += `<p class="source">${randomQuote.source}`;
+
+  if (randomQuote.citation) {
+    newQuote += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+
+  if (randomQuote.year) {
+    newQuote += `<span class="year">${randomQuote.year}</span>`;
+  }
+  newQuote += "</p>";
+  quoteBox.innerHTML = newQuote;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById("load-quote").addEventListener("click", printQuote, false);
